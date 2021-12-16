4f4254081347fb7af09235f2dd1a1c193c094be4
Nolann Biron <Nolann.biron@ynov.com>
2021-12-17 00:48:22 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
-var http = require('http');
+var https = require('https');
+const path = require('path');
+const fs = require('fs');
+
+const key = fs.readFileSync(path.join(__dirname, 'server.key'));
+const cert = fs.readFileSync(path.join(__dirname, 'server.cert'));
+ 
+const options = { key, cert };
 
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-  logger.info('Connected to MongoDB');
-  http.createServer(app).listen(3001, () => {
-    console.log('server running at ' + 3001)
+  logger.info('Connected to MongoDB'); 
+  https.createServer(options, app).listen(3001, () => {
+    console.log('App is running !');
   });
 });
 
