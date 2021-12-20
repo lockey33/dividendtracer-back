c40cb12cf3c9f1cebf949d809688822f6af0cdd8
Nolann Biron <Nolann.biron@ynov.com>
2021-12-20 14:23:42 +0100

    update certificates

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 
-let server;
+var privateKey  = fs.readFileSync(path.join(__dirname, 'letsencrypt/live/dividendtracer.com/privkey.pem'));
+var certificate = fs.readFileSync(path.join(__dirname, 'letsencrypt/live/dividendtracer.com/cert.pem'));
+
+var credentials = {key: privateKey, cert: certificate};
+
+let server = https.createServer(credentials, app);
+
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
-  server = app.listen(3001, () => {
+  server.listen(3001, () => {
     logger.info(`Listening to port 3001`);
   });
 });
